7f68561cbc1e1e1122c23aca6b8cee8f0574d241
Quidapchai <nquizx@gmail.com>
2024-11-01 13:08:33 +0700

    chore: fix compile error

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -5477,7 +5477,7 @@ int skill_castend_damage_id (struct block_list* src, struct block_list *bl, uint
 					(snatcher_skill=pc_checkskill(sd,RG_SNATCHER)) > 0 &&
 					(snatcher_skill*15 + 55) + pc_checkskill(sd,TF_STEAL)*10 > rnd()%1000) {
 					if(pc_steal_item(sd,bl,pc_checkskill(sd,TF_STEAL)))
-						clif_skill_nodamage(src,bl,TF_STEAL,snatcher_skill,1);
+						clif_skill_nodamage(src,*bl,TF_STEAL,snatcher_skill,1);
 					else
 						clif_skill_fail( *sd, RG_SNATCHER );
 				}
@@ -8044,7 +8044,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 		else
 			type = SC_ALCHEMYEFFICIENCY_BOTH;
 
-		clif_skill_nodamage(src, bl, skill_id, skill_lv,
+		clif_skill_nodamage(src, *bl, skill_id, skill_lv,
 			sc_start(src, bl, type, 100, skill_lv, skill_get_time(skill_id, skill_lv)));
 		break;
 
@@ -9629,7 +9629,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 		break;
 	case AM_TWILIGHT4:
 		if (sd) {
-			clif_skill_nodamage(src,bl,skill_id,skill_lv,1);
+			clif_skill_nodamage(src,*bl,skill_id,skill_lv,1);
 			if (!skill_produce_mix(sd, skill_id, ITEMID_BLUE_POTION, 0, 0, 0, 200, -1))
 				clif_skill_fail( *sd, skill_id );
 		}
@@ -9648,14 +9648,14 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 				clif_skill_fail( *sd, skill_id );
 				break;
 			}
-			clif_skill_nodamage(src,bl,skill_id,skill_lv,1);
+			clif_skill_nodamage(src,*bl,skill_id,skill_lv,1);
 			skill_produce_mix(sd, skill_id, ITEMID_ACID_BOTTLE, 0, 0, 0, 200, acid_idx-1);
 			skill_produce_mix(sd, skill_id, ITEMID_FIRE_BOTTLE, 0, 0, 0, 200, fire_idx-1);
 		}
 		break;
 	case AM_TWILIGHT6:
 		if (sd) {
-			clif_skill_nodamage(src,bl,skill_id,skill_lv,1);
+			clif_skill_nodamage(src,*bl,skill_id,skill_lv,1);
 			//Prepare 200 Slim White Potions.
 			if (!skill_produce_mix(sd, skill_id, ITEMID_COATING_BOTTLE, 0, 0, 0, 200, -1))
 				clif_skill_fail( *sd, skill_id );
@@ -10712,7 +10712,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 	case AM_CALLHOMUN:
 		if (sd && !sd->status.hom_id) {
 			clif_sendembryo(sd);
-			clif_skill_nodamage(src, bl, skill_id, skill_lv, 1);
+			clif_skill_nodamage(src, *bl, skill_id, skill_lv, 1);
 		}
 		else if (sd && !hom_call(sd, 0)) {
 			clif_skill_fail( *sd, skill_id );
